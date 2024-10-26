3bb99e9c9e5a196f8f633e2c64d378b28b4eddde
Mikhail <mike144513@gmail.com>
2024-10-26 20:32:25 +0500

    post and get

--- xhr.js
+++ xhr.js
@@ -2,6 +2,29 @@ const requestURL = 'https://jsonplaceholder.typicode.com/users';
 
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', requestURL)
+function sendRequest(method, url, body= null) {
+    return new Promise(() => {
+        xhr.open('GET', requestURL)
+        xhr.responseType = 'json'
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                console.log(xhr.response)
+            } else {
+                console.log(xhr.response);
+            }
+        }
+        xhr.onerror = () => {
+            console.log(xhr.response)
+        }
+        xhr.send(JSON.stringify(body))
+    })
+}
 
-xhr.send()
+// sendRequest('GET', requestURL,{
+const body = {
+    name:'Peter',
+    age: '20'
+}
+sendRequest('GET', requestURL, body)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
